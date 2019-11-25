d8e220f6807a010cf618a4e7fbf0399babc9209c
Julien Combattelli <julien.combattelli@gmail.com>
2019-11-25 23:50:57 +0100

    Change full bench output format

--- script/bench.sh
+++ script/bench.sh
@@ -9,7 +9,11 @@ CHAR_COUNT=(10    # 0-9
 
 for char_count in ${CHAR_COUNT[@]} ; do
     for input_size in ${INPUT_SIZE[@]} ; do
-        echo "Starting test with (input_size=$input_size, char_count=$char_count)"
+        echo "====================================================================================="
+	echo "Starting test with (input_size=$input_size, char_count=$char_count)"
         ../build/huffmanbench --benchmark_repetitions=10 --input_size=$input_size --char_count=$char_count
+	echo "====================================================================================="
+	echo
+	echo
     done
 done
