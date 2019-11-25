83188c7053caf452a4dadcd6aaf3c3cec2505dec
Julien Combattelli <julien.combattelli@gmail.com>
2019-11-25 23:59:32 +0100

    Swap input_size and char_count incr

--- script/bench.sh
+++ script/bench.sh
@@ -7,11 +7,11 @@ CHAR_COUNT=(10    # 0-9
             95    # all displaybles ascii chars
             255)  # a full byte extent
 
-for char_count in ${CHAR_COUNT[@]} ; do
-    for input_size in ${INPUT_SIZE[@]} ; do
-        echo "====================================================================================="
+for input_size in ${INPUT_SIZE[@]} ; do
+    for char_count in ${CHAR_COUNT[@]} ; do
+    echo "====================================================================================="
 	echo "Starting test with (input_size=$input_size, char_count=$char_count)"
-        ../build/huffmanbench --benchmark_repetitions=10 --input_size=$input_size --char_count=$char_count
+        ../build/huffmanbench --benchmark_repetitions=1 --input_size=$input_size --char_count=$char_count
 	echo "====================================================================================="
 	echo
 	echo
